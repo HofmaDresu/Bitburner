3c205a51c2d13f734d6a703e605cbddb3aa361a3
Matt Hoffman <hofmadresu@gmail.com>
2022-12-13 16:23:43 -0500

    Create manipulate-market.js

---
+++ stocks/manipulate-market.js
@@ -0,0 +1,12 @@
+import {serverToStock} from "/stocks/helpers.js";
+
+/** @param {NS} ns */
+export async function main(ns) {
+	while(true) {
+		// For each stock we own
+		// ignore if we're making money off the server directly
+		// weaken + either hack or grow
+		// loop
+		ns.sleep(1000);
+	}
+}
