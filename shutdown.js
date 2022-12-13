0507a95113af019e701afed938a80fd6dc3b4302
Matt Hoffman <hofmadresu@gmail.com>
2022-12-13 17:02:19 -0500

    Update shutdown.js

--- shutdown.js
+++ shutdown.js
@@ -1,3 +1,5 @@
+import {getStopableServers} from "/helpers.js";
+
 /** @param {NS} ns */
 export async function main(ns) {
 	var servers = getStopableServers(ns, "home");
@@ -5,15 +7,4 @@ export async function main(ns) {
 	servers.forEach((s) => {
 		ns.killall(s);
 	});
-}
-
-function getStopableServers(ns, currentServer, previousServer) {
-	var servers = ns.scan(currentServer).filter(s => s != previousServer);
-	if (!servers || servers.length === 0) return [];
-	return servers.flatMap((s) => {
-		var hasRootAccess = ns.hasRootAccess(s);
-
-		if (!hasRootAccess) { return };
-		return [s, ...getStopableServers(ns, s, currentServer)];
-	}).filter(s => s);
 }
