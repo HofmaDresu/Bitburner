2826a9f8951add9dce457c81493894a014daba5d
Matt Hoffman <hofmadresu@gmail.com>
2022-12-13 17:01:59 -0500

    Update run-money-maker.js

--- run-money-maker.js
+++ run-money-maker.js
@@ -1,9 +1,11 @@
+import {getStartableServers, getBestServersForHacking} from "/helpers.js";
+
 /** @param {NS} ns */
 export async function main(ns) {
 	let bestServersForHacking = [];
 	while (true) {
 		let myHackingLevel = ns.getHackingLevel();
-		let startableServers = getStartableServers(ns, "home", myHackingLevel);
+		let startableServers = getStartableServers(ns, "home", myHackingLevel, null, true);
 		let newBestServersForHacking = getBestServersForHacking(ns, startableServers, myHackingLevel);
 		if (bestServersForHacking.toString() !== newBestServersForHacking.toString()) {
 			bestServersForHacking = newBestServersForHacking;
@@ -16,48 +18,6 @@ export async function main(ns) {
 		}
 		await ns.sleep(60000);
 	}
-}
-
-function getStartableServers(ns, currentServer, myHackingLevel, previousServer) {
-	let hackablePorts = 0;
-	if (ns.fileExists('brutessh.exe')) hackablePorts++;
-	if (ns.fileExists('ftpcrack.exe')) hackablePorts++;
-	if (ns.fileExists('relaysmtp.exe')) hackablePorts++;
-	if (ns.fileExists('httpworm.exe')) hackablePorts++;
-	if (ns.fileExists('sqlinject.exe')) hackablePorts++;
-	const servers = ns.scan(currentServer).filter(s => s != previousServer);
-	if (!servers || servers.length === 0) return [];
-	return servers.flatMap((s) => {
-		let hasRootAccess = ns.hasRootAccess(s);
-
-		if (!hasRootAccess) {
-			let requiredHackingLevel = ns.getServerRequiredHackingLevel(s);
-			if (requiredHackingLevel > myHackingLevel) { return; }
-			let requiredPorts = ns.getServerNumPortsRequired(s);
-			if (requiredPorts > hackablePorts) { return; }
-			if (ns.fileExists('brutessh.exe')) ns.brutessh(s);
-			if (ns.fileExists('ftpcrack.exe')) ns.ftpcrack(s);
-			if (ns.fileExists('relaysmtp.exe')) ns.relaysmtp(s);
-			if (ns.fileExists('httpworm.exe')) ns.httpworm(s);
-			if (ns.fileExists('sqlinject.exe')) ns.sqlinject(s);
-			ns.nuke(s);
-		}
-
-		return [s, ...getStartableServers(ns, s, myHackingLevel, currentServer)];
-	}).filter(s => s).filter(async s => await ns.ps(s).length === 0).filter(s => s !== 'darkweb')
-	.sort((a, b) => ns.getServerMaxRam(b) - ns.getServerMaxRam(a));
-}
-
-function getBestServersForHacking(ns, startableServers, myHackingLevel) {
-	const eligibleServers = startableServers.filter(server => {
-		var requiredHackingLevel = ns.getServerRequiredHackingLevel(server);
-		return requiredHackingLevel < myHackingLevel / 3 && ns.getServerMaxMoney(server) > 0;
-	});
-
-	const numberOfServersToHack = Math.min(Math.ceil(startableServers.length / 10), eligibleServers.length);
-
-	const orderedServers = eligibleServers.sort((a, b) => ns.getServerMaxMoney(b) - ns.getServerMaxMoney(a));
-	return orderedServers.slice(0, numberOfServersToHack - 1);
 }
 
 async function stopServerIfRetargetNeeded(ns, startableServers, bestServerForHacking) {
