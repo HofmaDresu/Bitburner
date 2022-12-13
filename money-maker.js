e746ecdf1330519c37f71469b19298fd37b1803e
Matt Hoffman <hofmadresu@gmail.com>
2022-12-13 14:06:11 -0500

    Create money-maker.js

---
+++ money-maker.js
@@ -0,0 +1,28 @@
+export async function main(ns) {
+	const server = arguments[0].args[0];
+	while(true) {
+		await makeMoneyFromServer(ns, server);
+	}
+}
+
+async function makeMoneyFromServer(ns, server) {
+	await prepServerForHack(ns, server);
+	await ns.hack(server);
+}
+
+async function prepServerForHack(ns, server) {
+	const maxServerMoney = await ns.getServerMaxMoney(server);
+	let currentServerMoney = await ns.getServerMoneyAvailable(server);
+	while (currentServerMoney < maxServerMoney * .75) {
+		await ns.grow(server);
+		await weaken(ns, server);
+		currentServerMoney = await ns.getServerMoneyAvailable(server);
+	}	
+}
+
+async function weaken(ns, server) {
+	const minSecurityLevel = ns.getServerMinSecurityLevel(server);
+	while (await ns.getServerSecurityLevel(server) > minSecurityLevel + 1) {
+		await ns.weaken(server);
+	}
+}
