1b7aafddacbc4ab3b2dc16ab44bf590cb5bcafe9
Matt Hoffman <hofmadresu@gmail.com>
2022-12-14 19:55:02 -0500

    Update startup.js

--- startup.js
+++ startup.js
@@ -6,12 +6,14 @@ export async function main(ns) {
 	ns.run('purchase-server.js')
 	ns.run('/stocks/stock-watcher.js');
 	ns.run('/stocks/play-the-market.js');
+	ns.run('/stocks/manipulate-market.js', 100);
 	var myHackingLevel = ns.getHackingLevel();
 	var startableServers = getStartableServers(ns, "home", myHackingLevel);
 	var bestServerForHacking = getBestServersForHacking(ns, startableServers, myHackingLevel)[0];
 
 	var startServerRam = ns.getScriptRam('start-server.js');
-	var availableMemory = ns.getServerMaxRam("home") - ns.getServerUsedRam("home") - (startServerRam * 2);
+	// SUPER inefficient
+	var availableMemory = ns.getServerMaxRam("home") - ns.getServerUsedRam("home") - (startServerRam * startableServers.length);
 	var moneyMakerRam = ns.getScriptRam('money-maker.js');
 	var maxThreads = Math.floor(availableMemory / moneyMakerRam);
 	ns.run('money-maker.js', maxThreads, bestServerForHacking);	
