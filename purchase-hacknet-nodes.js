35764dc38e33092ee66b4c9e4e7400d50daf7e5a
Matt Hoffman <hofmadresu@gmail.com>
2022-12-18 22:45:44 -0500

    Create purchase-hacknet-nodes.js

---
+++ purchase-hacknet-nodes.js
@@ -0,0 +1,48 @@
+const maxNodes = 20;
+/** @param {NS} ns */
+export async function main(ns) {	
+	for (let nodeIndex = 0; nodeIndex <= maxNodes; nodeIndex++) {
+		if (!ns.fileExists('formulas.exe')) {
+			await ns.sleep(60000);
+			continue;
+		}
+
+		if (nodeIndex >=  ns.hacknet.numNodes()) {
+			while(getSpendableMoney(ns) <= ns.hacknet.getPurchaseNodeCost()) {
+				await ns.sleep(60000);
+			}
+			ns.hacknet.purchaseNode();
+		}
+
+		while (ns.hacknet.getNodeStats(nodeIndex).level < 200) {
+			if (getSpendableMoney(ns) >= ns.hacknet.getLevelUpgradeCost(nodeIndex, 1)) {
+				ns.hacknet.upgradeLevel(nodeIndex, 1);
+				await ns.sleep(100);
+			} else {
+				await ns.sleep(60000);
+			}
+		}
+		while (ns.hacknet.getNodeStats(nodeIndex).ram < 60) {
+			if (getSpendableMoney(ns) >= ns.hacknet.getRamUpgradeCost(nodeIndex, 1)) {
+				ns.hacknet.upgradeRam(nodeIndex, 1);
+				await ns.sleep(100);
+			} else {
+				await ns.sleep(60000);
+			}
+		}
+		while (ns.hacknet.getNodeStats(nodeIndex).cores < 16) {
+			if (getSpendableMoney(ns) >= ns.hacknet.getCoreUpgradeCost(nodeIndex, 1)) {
+				ns.hacknet.upgradeCore(nodeIndex, 1);
+				await ns.sleep(100);
+			} else {
+				await ns.sleep(60000);
+			}
+		}
+
+		await ns.sleep(100);
+	}
+}
+
+function getSpendableMoney(ns) {
+	return .8 * ns.getServerMoneyAvailable("home");
+}
