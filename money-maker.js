a3221443e8231eab9c1870e6629cf10c2320b5a8
Matt Hoffman <hofmadresu@gmail.com>
2022-12-18 22:45:19 -0500

    Update money-maker.js

--- money-maker.js
+++ money-maker.js
@@ -1,25 +1,15 @@
-import {weakenToMin} from '/helpers.js';
+import {weakenToMin, hackToTargetPercent, growToTargetPercent} from "/helpers.js";
 
 /** @param {NS} ns */
 export async function main(ns) {
 	const server = arguments[0].args[0];
-	await weaken(ns, server);
+	await weakenToMin(ns, server);
 	while(true) {
 		await makeMoneyFromServer(ns, server);
 	}
 }
 
 async function makeMoneyFromServer(ns, server) {
-	await prepServerForHack(ns, server);
-	await ns.hack(server);
-}
-
-async function prepServerForHack(ns, server) {
-	const maxServerMoney = await ns.getServerMaxMoney(server);
-	let currentServerMoney = await ns.getServerMoneyAvailable(server);
-	while (currentServerMoney < maxServerMoney * .75) {
-		await ns.grow(server);
-		await weakenToMin(ns, server);
-		currentServerMoney = await ns.getServerMoneyAvailable(server);
-	}	
+	await growToTargetPercent(ns, server, 1, false);
+	await hackToTargetPercent(ns, server, .50, false);
 }
