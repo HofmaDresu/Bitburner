d1552821776a5ade75add55477b3259352473290
Matt Hoffman <hofmadresu@gmail.com>
2022-12-23 21:26:16 -0500

    Update startup.js

--- startup.js
+++ startup.js
@@ -3,24 +3,29 @@ import {getStartableServers, getBestServersForHacking} from "/helpers.js"
 /** @param {NS} ns */
 export async function main(ns) {
 	ns.run('run-money-maker.js');
-	ns.run('purchase-server.js')
-	ns.run('purchase-hacknet-nodes.js')
-	ns.run('/stocks/stock-watcher.js');
-	ns.run('/stocks/activate-purchases.js');
-	ns.run('/stocks/play-the-market.js');
-	// Always call this last
-	ns.run('/stocks/start-manipulating-market.js');
+	ns.run('purchase-server.js');
+	ns.run('purchase-programs.js');
+	ns.run('upgrade-home-server.js');
+	// Not worth it right now, re-enable when we have the right bitnode
+	//ns.run('purchase-hacknet-nodes.js') 
+	if (ns.getServerMaxRam("home") >= 1024) {
+		ns.run('/stocks/stock-watcher.js');
+		ns.run('/stocks/activate-purchases.js');
+		ns.run('/stocks/play-the-market.js');
+		// Always call this last
+		ns.run('/stocks/start-manipulating-market.js');
+	}
 	/*
-	await ns.sleep(10000);
+	await ns.sleep(60000);
 	var myHackingLevel = ns.getHackingLevel();
-	var startableServers = getStartableServers(ns, "home", myHackingLevel);
+	var startableServers = await getStartableServers(ns, "home", myHackingLevel);
 	var bestServerForHacking = getBestServersForHacking(ns, startableServers, myHackingLevel)[0];
 
 	var startServerRam = ns.getScriptRam('start-server.js');
 	// SUPER inefficient
 	var availableMemory = ns.getServerMaxRam("home") - ns.getServerUsedRam("home") - (startServerRam * startableServers.length);
 	var moneyMakerRam = ns.getScriptRam('money-maker.js');
-	var maxThreads = Math.floor(availableMemory / moneyMakerRam);
-	// ns.run('money-maker.js', maxThreads, bestServerForHacking);	
-	*/
+	var maxThreads = Math.max(Math.floor(availableMemory / moneyMakerRam), 1);
+	 ns.run('money-maker.js', maxThreads, bestServerForHacking);
+	 */
 }
