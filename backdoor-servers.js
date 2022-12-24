a84c328702bfccb22518c62c6999f6c0f7d4b8bd
Matt Hoffman <hofmadresu@gmail.com>
2022-12-23 21:22:46 -0500

    Create backdoor-servers.js

---
+++ backdoor-servers.js
@@ -0,0 +1,23 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	var servers = ns.scan("home");
+	for (let i = 0; i < servers.length; i++) {
+		await backdoorServersRecursive(ns, servers[i], "home");
+		ns.singularity.connect("home");
+	}
+	ns.tprint("DONE!");
+}
+
+/** @param {NS} ns */
+async function backdoorServersRecursive(ns, currentServer, previousServer) {
+	ns.print(`${currentServer} <- ${previousServer}`)
+	if (!ns.hasRootAccess(currentServer)) return;
+	ns.singularity.connect(currentServer);
+	await ns.singularity.installBackdoor();
+	var servers = ns.scan(currentServer).filter(s => s != previousServer);
+	if (!servers || servers.length === 0) return;
+	for (let i = 0; i < servers.length; i++) {
+		await backdoorServersRecursive(ns, servers[i], currentServer);
+	}
+	ns.singularity.connect(currentServer);
+}
