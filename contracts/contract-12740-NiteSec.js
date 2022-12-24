3a0c8fff80bb9be4caafc2a59d084aaf4c1064bf
Matt Hoffman <hofmadresu@gmail.com>
2022-12-24 12:38:37 -0500

    start working on summations contract

--- contracts/contract-12740-NiteSec.js
+++ contracts/contract-12740-NiteSec.js
@@ -1,9 +1,23 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    const targetNum = 50;
+    const targetNum = arguments[0].args[0];
+
+    const summations = [[targetNum - 1, 1]];
+
+    while (summations[summations.length -1].length < targetNum) {
+        const mostRecentSummation = summations[summations.length -1];
+        const nonOneDigits = mostRecentSummation.filter(u => u > 1);
+        const lastNonOneDigit = nonOneDigits[nonOneDigits.length-1];
+        const oneDigits = mostRecentSummation.filter(u => u === 1);
+
+        summations.push([...nonOneDigits.slice(0, nonOneDigits.length-2), lastNonOneDigit -1, 1, ...oneDigits]);
+    }
+
+    ns.tprint(summations)
 
 
-    ns.tprint(profit);
+
+    ns.tprint(summations.length);
     //ns.tprint(ns.codingcontract.attempt(profit, "contract-12740-NiteSec.cct", "computek", {returnReward: true}));
 }
 
