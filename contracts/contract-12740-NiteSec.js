658e1ee3d5778ab8dd4570ab61de99d59b5fcbe6
Matt Hoffman <hofmadresu@gmail.com>
2022-12-24 11:02:54 -0500

    start new contract

---
+++ contracts/contract-12740-NiteSec.js
@@ -0,0 +1,9 @@
+/** @param {NS} ns */
+export async function main(ns) {
+    const targetNum = 50;
+
+
+    ns.tprint(profit);
+    //ns.tprint(ns.codingcontract.attempt(profit, "contract-12740-NiteSec.cct", "computek", {returnReward: true}));
+}
+
