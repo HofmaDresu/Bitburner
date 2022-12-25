7cf479a15c47cd615e23f3df1b941f32b8e1abd7
Matt Hoffman <hofmadresu@gmail.com>
2022-12-25 14:19:32 -0500

    hack to handle edge case + other tweaks that hopefully work

--- contracts/spiralize-matrix.js
+++ contracts/spiralize-matrix.js
@@ -11,23 +11,28 @@ export async function main(ns) {
     let currentHeightIndex = 0;
     let movementType = "w";
     let movementDirection = 1;
+    const expectedLength = matrix.flatMap(i => i).length;
+    /*
+    for (let i = 0; i < matrix.length; i++) {
+        ns.tprint(matrix[i].map(x => `${x}`.padStart(2, "0")).join(","))
+    }
+*/
 
+    let spiralizedMatrix = [];
 
-    const spiralizedMatrix = [];
-
-    while (minWidthIndex < maxWidthIndex && minHeightIndex < maxHeightIndex) {
+    while (minWidthIndex < maxWidthIndex || minHeightIndex < maxHeightIndex -1) {
         spiralizedMatrix.push(matrix[currentHeightIndex][currentWidthIndex]);
 
         if (movementType === "w") {
             const proposedIndex = currentWidthIndex + movementDirection;
             if (proposedIndex >= maxWidthIndex) {
                 movementType = "h";
-                maxWidthIndex--;
                 currentHeightIndex += movementDirection;
                 minHeightIndex++;
             } else if (proposedIndex < minWidthIndex) {
                 movementType = "h";
                 currentHeightIndex += movementDirection;
+                maxHeightIndex--;
             } else {
                 currentWidthIndex = proposedIndex;
             }
@@ -35,9 +40,9 @@ export async function main(ns) {
             const proposedIndex = currentHeightIndex + movementDirection;
             if (proposedIndex >= maxHeightIndex) {
                 movementType = "w";
-                maxHeightIndex--;
                 movementDirection *= -1;
                 currentWidthIndex += movementDirection;
+                maxWidthIndex--;
             } else if (proposedIndex < minHeightIndex) {
                 movementType = "w";
                 movementDirection *= -1;
@@ -49,8 +54,9 @@ export async function main(ns) {
         }
     }
 
-    ns.tprint(spiralizedMatrix);
     
+    // Horrible hack
+    spiralizedMatrix = spiralizedMatrix.slice(0, expectedLength)
     
     const result = ns.codingcontract.attempt(spiralizedMatrix, contractFileName, targetServer, {returnReward: true});
     if (result) {
