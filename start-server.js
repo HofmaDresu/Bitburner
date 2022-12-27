a3b84213415a85f7a003a5d6e3c31394bdfb4578
Matt Hoffman <hofmadresu@gmail.com>
2022-12-27 15:31:10 -0500

    Use v2 on all servers

--- start-server.js
+++ start-server.js
@@ -1,17 +1,17 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	var targetServer = arguments[0].args[0];
-	var servers = arguments[0].args.slice(1);
-	var script = 'money-maker.js';
+	var server = arguments[0].args[1];
+	var script = '/money-maker/money-maker-v2.js';
 	var scriptRam = ns.getScriptRam(script);
-	servers.forEach(server => {
-		var hasRootAccess = ns.hasRootAccess(server);
-		if (!hasRootAccess) return;
-		ns.scp('helpers.js', server);
-		ns.scp('/stocks/helpers.js', server);
-		ns.scp(script, server);
-		var availableMemory = ns.getServerMaxRam(server);
-		var maxThreads = Math.floor(availableMemory / scriptRam);	
-		if (maxThreads > 0) ns.exec(script, server, maxThreads, targetServer);
-	});
+	var hasRootAccess = ns.hasRootAccess(server);
+	if (!hasRootAccess) return;
+	ns.scp('helpers.js', server);
+	ns.scp(script, server);		
+	ns.scp('/money-maker/grow-server.js', server);
+	ns.scp('/money-maker/hack-server.js', server);
+	ns.scp('/money-maker/weaken-server.js', server);
+	var availableMemory = ns.getServerMaxRam(server);
+	var maxThreads = Math.floor(availableMemory / scriptRam);	
+	if (maxThreads > 0) ns.exec(script, server, 1, targetServer);
 }
