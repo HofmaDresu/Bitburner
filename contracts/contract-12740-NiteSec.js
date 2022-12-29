a5e7278a345a58f2a7a9677e6a76bfd0e2c2e82d
Matt Hoffman <hofmadresu@gmail.com>
2022-12-29 10:02:21 -0500

    maybe progress on summations?

--- contracts/contract-12740-NiteSec.js
+++ contracts/contract-12740-NiteSec.js
@@ -2,22 +2,60 @@
 export async function main(ns) {
     const targetNum = arguments[0].args[0];
 
-    const summations = [[targetNum - 1, 1]];
+    const currentSummation = [targetNum - 1, 1];
+    const ones = 0;
 
-    while (summations[summations.length -1].length < targetNum) {
-        const mostRecentSummation = summations[summations.length -1];
-        const nonOneDigits = mostRecentSummation.filter(u => u > 1);
-        const lastNonOneDigit = nonOneDigits[nonOneDigits.length-1];
-        const oneDigits = mostRecentSummation.filter(u => u === 1);
-
-        summations.push([...nonOneDigits.slice(0, nonOneDigits.length-2), lastNonOneDigit -1, 1, ...oneDigits]);
-    }
+    const summations = findSummations(ns, targetNum, currentSummation, ones, 1);
 
     ns.tprint(summations)
 
 
-
-    ns.tprint(summations.length);
     //ns.tprint(ns.codingcontract.attempt(profit, "contract-12740-NiteSec.cct", "computek", {returnReward: true}));
 }
 
+// TODO: Make iterative
+
+function findSummations(ns, targetSum, currentSummation, ones, numberOfSummations) {
+    ns.tprint(`${currentSummation} ones: ${ones}`)
+    if (ones === targetSum) return numberOfSummations
+    if (currentSummation.length === 1 && currentSummation[currentSummation.length-1] === 1) return numberOfSummations;
+    if (currentSummation.length === currentSummation.filter(x => x === 1).length) return numberOfSummations;
+
+    if (currentSummation[currentSummation.length-1] !== 1 && ones === currentSummation[currentSummation.length-1]) {
+        //ns.tprint("new")
+        currentSummation.push(ones);
+        ones = 0;
+        return findSummations(ns, targetSum, currentSummation, ones, numberOfSummations + 1);
+    } else if (currentSummation[currentSummation.length-1] > 1) {
+       //ns.tprint("Last digit not one")
+        currentSummation[currentSummation.length-1]--;
+        ones++;
+        return findSummations(ns, targetSum, currentSummation, ones, numberOfSummations + 1);
+    } else {
+        //ns.tprint("rebuild")
+        // Only rebuilds last number, not subsequent
+        currentSummation = currentSummation.slice(0, currentSummation.length-1);
+        currentSummation[currentSummation.length-1]--;
+
+        for (let i = currentSummation[currentSummation.length-1]; i > 1;){
+            const partialSum = currentSummation.reduce((prev, curr) => prev += curr);
+            if (partialSum + i <= targetSum) {
+                currentSummation.push(i);
+            } else {
+                i--;
+            }
+        }
+        
+        ones = targetSum - currentSummation.reduce((prev, curr) => prev += curr);
+
+        // Causes crash
+        /*
+        while (ones >= currentSummation[currentSummation.length-1]) {
+            ones -= currentSummation[currentSummation.length-1];
+            currentSummation.push(currentSummation[currentSummation.length-1]);
+        }
+        */
+
+        return findSummations(ns, targetSum, currentSummation, ones, numberOfSummations + 1);
+    }
+}
