540e2d4cc0620f10bec329a0d29969a452fce5e3
Matt Hoffman <hofmadresu@gmail.com>
2022-12-31 11:22:22 -0500

    start getting game types

--- infiltration/infiltration-monitor.js
+++ infiltration/infiltration-monitor.js
@@ -1,8 +1,6 @@
 /** @param {NS} ns */
 export async function main(ns) {
 	while(true) {
-        // Look for 
-        // <button class="MuiButton-root MuiButton-text MuiButton-textPrimary MuiButton-sizeMedium MuiButton-textSizeMedium MuiButtonBase-root css-1b57hxc" tabindex="0" type="button">Cancel Infiltration<span class="MuiTouchRipple-root css-w0pj6f">
         let inInfiltrationMode = false;
         const potentialInfiltrationGameWindows = document.getElementsByClassName("jss1 MuiBox-root css-0");
         let infiltrationGameWindow = null;
@@ -15,16 +13,28 @@ export async function main(ns) {
             }
         }
 
-        ns.print(`In infiltration mode ${inInfiltrationMode}`);
+        //ns.print(`In infiltration mode ${inInfiltrationMode}`);
 
+        if (inInfiltrationMode) {
+            // If we're in infiltration, look for the following to find the current game
+            // <h4 class="MuiTypography-root MuiTypography-h4 css-qan7cn">Type it backward</h4>
 
-        // If that's there, we've started an infiltration
-        // If we're in infiltration, look for the following to find the current game
-        // <h4 class="MuiTypography-root MuiTypography-h4 css-qan7cn">Type it backward</h4>
-        // Note: there will be more than 1 h4 like this. Ignore any that don't match a game we know about
-        // Switch on current game to have other files play the game
-        // wait for them to finish
+            const potentialGameNames = infiltrationGameWindow.getElementsByClassName("MuiTypography-root MuiTypography-h4 css-qan7cn");
+            for (let i = 0; i < potentialGameNames.length; i++) {
+                ns.print(potentialGameNames[i].innerHTML);
+                switch(potentialGameNames[i].innerHTML) {
+                    case "Type it backward":
+                        break;
+                    case "Slash when his guard is down!":
+                        break;
+                }
+            }
 
+            // Note: there will be more than 1 h4 like this. Ignore any that don't match a game we know about
+            // Switch on current game to have other files play the game
+            // wait for them to finish
+
+        }
         await ns.sleep(500);
     }
 }
