548d359efdf7e72f695fd6bd224a5db3e702a4c0
Matt Hoffman <hofmadresu@gmail.com>
2023-01-02 13:29:00 -0500

    begin wrapping event handlers

--- infiltration/infiltration-monitor.js
+++ infiltration/infiltration-monitor.js
@@ -3,6 +3,7 @@ import {typeItBackward} from "infiltration/type-it-backward";
 /** @param {NS} ns */
 export async function main(ns) {
     ns.disableLog("sleep")
+    wrapEventListeners();
 	while(true) {
         let inInfiltrationMode = false;
         const potentialInfiltrationGameWindows = document.getElementsByClassName("jss1 MuiBox-root css-0");
@@ -41,4 +42,55 @@ export async function main(ns) {
         }
         await ns.sleep(500);
     }
+}
+
+// Adapted from https://pastebin.com/7DuFYDpJ
+function wrapEventListeners() {
+    if (!document._addEventListener) {
+		document._addEventListener = document.addEventListener;
+
+		document.addEventListener = function (type, callback, options) {
+			if ("undefined" === typeof options) {
+				options = false;
+			}
+			let handler = false;
+
+			// For this script, we only want to modify "keydown" events.
+			if ("keydown" === type) {
+				handler = function (...args) {
+					if (!args[0].isTrusted) {
+						const hackedEv = {};
+
+						for (const key in args[0]) {
+							if ("isTrusted" === key) {
+								hackedEv.isTrusted = true;
+							} else if ("function" === typeof args[0][key]) {
+								hackedEv[key] = args[0][key].bind(args[0]);
+							} else {
+								hackedEv[key] = args[0][key];
+							}
+						}
+
+						args[0] = hackedEv;
+					}
+
+					return callback.apply(callback, args);
+				};
+
+				for (const prop in callback) {
+					if ("function" === typeof callback[prop]) {
+						handler[prop] = callback[prop].bind(callback);
+					} else {
+						handler[prop] = callback[prop];
+					}
+				}
+			}
+
+			return this._addEventListener(
+				type,
+				handler ? handler : callback,
+				options
+			);
+		};
+	}
 }
