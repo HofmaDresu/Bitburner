9be059812bbfacd2b2d70a054614c4bea02882cf
Matt Hoffman <hofmadresu@gmail.com>
2023-01-07 11:53:17 -0500

    use updated player in calculations

--- money-maker/money-maker-v2.js
+++ money-maker/money-maker-v2.js
@@ -19,7 +19,6 @@ export async function main(ns) {
 
 /** @param {NS} ns */
 async function advancedMakeMoneyFromServer(ns, server) {
-	const player = ns.getPlayer();
 
 	const hostname = ns.getServer().hostname;
 	var availableMemory = (ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname));
@@ -27,25 +26,25 @@ async function advancedMakeMoneyFromServer(ns, server) {
 	const growScript = '/money-maker/grow-server.js';
 	var growRam = ns.getScriptRam(growScript);
 	var maxGrowThreads = Math.max(Math.floor(availableMemory / growRam), 1);
-	const calcGrowTime = (targetServer) => ns.formulas.hacking.growTime(targetServer, player);
+	const calcGrowTime = (targetServer, player) => ns.formulas.hacking.growTime(targetServer, player);
 	const calcGrowThreads = () => calculateThreadsForGrowToTargetPercent(ns, server, 1, maxGrowThreads);
-	await actionServer(ns, player, server, growScript, calcGrowTime, calcGrowThreads)
+	await actionServer(ns, server, growScript, calcGrowTime, calcGrowThreads)
 
 	const hackScript = '/money-maker/hack-server.js';
 	var hackRam = ns.getScriptRam(hackScript);
 	var maxHackThreads = Math.max(Math.floor(availableMemory / hackRam), 1);
-	const calcHackTime = (targetServer) => ns.formulas.hacking.hackTime(targetServer, player);
+	const calcHackTime = (targetServer, player) => ns.formulas.hacking.hackTime(targetServer, player);
 	const calcHackThreads = () => calculateThreadsForHackToTargetPercent(ns, server, .5, maxHackThreads);
-	await actionServer(ns, player, server, hackScript, calcHackTime, calcHackThreads)
+	await actionServer(ns, server, hackScript, calcHackTime, calcHackThreads)
 }
 
 /** @param {NS} ns */
-async function actionServer(ns, player, targetServerHostname, script, calcActionTime, calcActionThreads) {
+async function actionServer(ns, targetServerHostname, script, calcActionTime, calcActionThreads) {
 	const hostname = ns.getServer().hostname;
 	
 	let threadsNeededForAction = calcActionThreads();
 	while (threadsNeededForAction > 0) {
-		const actionTime = await weakenToMin(ns, targetServerHostname, player, hostname, script, threadsNeededForAction, calcActionTime);
+		const actionTime = await weakenToMin(ns, targetServerHostname, hostname, script, threadsNeededForAction, calcActionTime);
 		ns.run(script, threadsNeededForAction, targetServerHostname, threadsNeededForAction);
 		await ns.sleep(actionTime);
 		threadsNeededForAction = calcActionThreads();
@@ -53,7 +52,8 @@ async function actionServer(ns, player, targetServerHostname, script, calcAction
 }
 
 /** @param {NS} ns */
-async function weakenToMin(ns, targetServerHostname, player, hostname, script, threadsNeededForAction, calcActionTime) {
+async function weakenToMin(ns, targetServerHostname, hostname, script, threadsNeededForAction, calcActionTime) {
+	let player = ns.getPlayer();
 	const availableMemory = (ns.getServerMaxRam(hostname) - ns.getServerUsedRam(hostname));
 	const actionRam = ns.getScriptRam(script);
 	const weakenRam = ns.getScriptRam('/money-maker/weaken-server.js');
@@ -61,7 +61,7 @@ async function weakenToMin(ns, targetServerHostname, player, hostname, script, t
 	const threadsNeededToWeaken = await calculateThreadsToWeakenToMin(ns, targetServerHostname, maxWeakenThreads);
 	const numberOfWeakensNeeded = Math.ceil((threadsNeededToWeaken * 1.0) / maxWeakenThreads);
 	let targetServer = ns.getServer(targetServerHostname);
-	let actionTime = calcActionTime(targetServer);
+	let actionTime = calcActionTime(targetServer, player);
 
 	for (let i = 0; i < numberOfWeakensNeeded; i++) {
 		const timeToWeaken = ns.formulas.hacking.weakenTime(targetServer, player) + PADDING_TIME;
@@ -70,7 +70,8 @@ async function weakenToMin(ns, targetServerHostname, player, hostname, script, t
 		const sleepTime = i < numberOfWeakensNeeded - 1 ? timeToWeaken : timeToWeakenLastRun;
 		await ns.sleep(Math.max(sleepTime, 10));
 		targetServer = ns.getServer(targetServerHostname);
-		actionTime = calcActionTime(targetServer);
+		player = ns.getPlayer();
+		actionTime = calcActionTime(targetServer, player);
 	}
 
 	return actionTime + PADDING_TIME;
