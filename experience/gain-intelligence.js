b9601ff31387476bbb6291a3719c767219b8e991
Matt Hoffman <hofmadresu@gmail.com>
2023-01-13 18:02:18 -0500

    More even cpu usage

--- experience/gain-intelligence.js
+++ experience/gain-intelligence.js
@@ -2,18 +2,12 @@
 export async function main(ns) {
 
 	while (true) {
-		const server = ns.getServer(); //TODO: this is getting home because that's where this script runs. Can we get "current connected server"? Do we need to run multiple threads?
 		var availableMemory = (ns.getServerMaxRam("home") - ns.getServerUsedRam("home")) * .8;
 		const manualHackRam = ns.getScriptRam('/experience/manual-hack-self.js');
 		const hackThreads = Math.floor(availableMemory / manualHackRam);
-		const player = ns.getPlayer();
-		const timeToHack = ns.formulas.hacking.hackTime(server, player); 
-		const timeToWeaken = ns.formulas.hacking.weakenTime(server, player) ;
 		if (hackThreads > 0) {
-			for (let i = 0; i < hackThreads; i++) {
-				ns.run("/experience/manual-hack-self.js", 1);
-			}
+			ns.run("/experience/manual-hack-self.js", 1);
 		}
-		await ns.sleep(timeToHack + timeToWeaken + 50);
+		await ns.sleep(1);
 	}
 }
