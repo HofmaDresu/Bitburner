3d6af3d25958bf23dd6f2643d1ab1d5dd106edb2
Matt Hoffman <hofmadresu@gmail.com>
2023-01-15 11:14:14 -0500

    More better intelligence gain

--- experience/gain-intelligence.js
+++ experience/gain-intelligence.js
@@ -1,14 +1,10 @@
 /** @param {NS} ns */
 export async function main(ns) {
-
-	while (true) {
-		const minMemory = ns.getServerMaxRam("home") * .1
-		let availableMemory = Math.max(ns.getServerMaxRam("home") - ns.getServerUsedRam("home") - minMemory, 0);
-		const manualHackRam = ns.getScriptRam('/experience/manual-hack-self.js');
-		const hackThreads = Math.floor(availableMemory / manualHackRam);
-		for(let i = 0; i < Math.min(1000, hackThreads); i++) {
-			ns.run("/experience/manual-hack-self.js", 1);
-		}
-		await ns.sleep(.001);
+	const minMemory = ns.getServerMaxRam("home") * .1
+	let availableMemory = Math.max(ns.getServerMaxRam("home") - ns.getServerUsedRam("home") - minMemory, 0);
+	const manualHackRam = ns.getScriptRam('/experience/manual-hack-self.js');
+	const hackThreads = Math.floor(availableMemory / manualHackRam);
+	for(let i = 0; i < hackThreads; i++) {
+		ns.run("/experience/manual-hack-self.js", 1);
 	}
 }
--- experience/manual-hack-self.js
+++ experience/manual-hack-self.js
@@ -1,5 +1,7 @@
 /** @param {NS} ns */
 export async function main(ns) {
-    await ns.weaken(ns.getHostname());
-    await ns.singularity.manualHack();
+    while(true) {
+        await ns.weaken(ns.getHostname());
+        await ns.singularity.manualHack();
+    }
 }
