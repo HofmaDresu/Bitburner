08f4c918e21d4205e187440ff04190a1179553c2
Matt Hoffman <hofmadresu@gmail.com>
2023-01-16 21:42:57 -0500

    protect my real PC

--- experience/gain-intelligence.js
+++ experience/gain-intelligence.js
@@ -3,8 +3,9 @@ export async function main(ns) {
 	const minMemory = ns.getServerMaxRam("home") * .1
 	let availableMemory = Math.max(ns.getServerMaxRam("home") - ns.getServerUsedRam("home") - minMemory, 0);
 	const manualHackRam = ns.getScriptRam('/experience/manual-hack-self.js');
-	const hackThreads = Math.floor(availableMemory / manualHackRam);
+	const hackThreads = Math.min(Math.floor(availableMemory / manualHackRam), 1000);
 	for(let i = 0; i < hackThreads; i++) {
 		ns.run("/experience/manual-hack-self.js", 1);
+		await ns.sleep(1);
 	}
 }
