117aa375e69fbb21934a4f824cc89e1d5aca7000
Matt Hoffman <hofmadresu@gmail.com>
2023-01-17 17:35:57 -0500

    Safer stock checks

--- stocks/helpers.js
+++ stocks/helpers.js
@@ -43,6 +43,7 @@ export const stockPriceFileName = "/stocks/stock-database.txt";
 
 /** @param {NS} ns */
 export function shouldLowerValueForStock(ns, stockSymbol) {	
+    if (!ns.stock.hasWSEAccount()) return true;
     if (!stockSymbol) return false;
 	const [longShares, _longPx, _shortShares, _shortPx] = ns.stock.getPosition(stockSymbol);
 	return longShares === 0;
@@ -50,6 +51,7 @@ export function shouldLowerValueForStock(ns, stockSymbol) {
 
 /** @param {NS} ns */
 export function shouldRaiseValueForStock(ns, stockSymbol) {	
+    if (!ns.stock.hasWSEAccount()) return false;
     if (!stockSymbol) return false;
 	const [longShares, _longPx, shortShares, _shortPx] = ns.stock.getPosition(stockSymbol);
 	if (longShares === 0 && shortShares === 0) return false; // Don't care
