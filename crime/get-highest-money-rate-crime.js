fa8397bc6cc834889bb57a075ed9f2d85ca57cc8
Matt Hoffman <hofmadresu@gmail.com>
2023-01-18 07:47:39 -0500

    Better calculation

--- crime/get-highest-money-rate-crime.js
+++ crime/get-highest-money-rate-crime.js
@@ -3,5 +3,7 @@
 export async function main(ns) {
     const crimes = ["Shoplift", "Rob Store", "Mug", "Larceny", "Deal Drugs", "Bond Forgery", "Traffick Arms", "Homicide", "Grand Theft Auto", "Kidnap", "Assassination", "Heist"];
 
-    ns.tprint(crimes.map(c => ns.singularity.getCrimeStats(c)).sort((a, b) => (b.money / b.time) - (a.money - a.time))[0].type);
+    ns.tprint(crimes.map(c => ns.singularity.getCrimeStats(c))
+        .map(c => ({type: c.type, money_over_time: (c.money / c.time) * ns.singularity.getCrimeChance(c.type)}))
+        .sort((a, b) => b.money_over_time - a.money_over_time)[0].type);
 }
