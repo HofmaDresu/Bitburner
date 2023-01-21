f2e0042e8d1d302077cb858cc6cde2e3535cc833
Matt Hoffman <hofmadresu@gmail.com>
2023-01-21 14:14:47 -0500

    get best karma rate

---
+++ crime/get-best-karma-rate-crime.js
@@ -0,0 +1,9 @@
+
+/** @param {NS} ns */
+export async function main(ns) {
+    const crimes = ["Shoplift", "Rob Store", "Mug", "Larceny", "Deal Drugs", "Bond Forgery", "Traffick Arms", "Homicide", "Grand Theft Auto", "Kidnap", "Assassination", "Heist"];
+
+    ns.tprint(crimes.map(c => ns.singularity.getCrimeStats(c))
+        .map(c => ({type: c.type, karma_over_time: (c.karma / c.time) * ns.singularity.getCrimeChance(c.type)}))
+        .sort((a, b) => b.karma_over_time - a.karma_over_time)[0].type);
+}
