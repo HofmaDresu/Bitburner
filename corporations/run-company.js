210e02ca796ad2f871e18d4624091ef856fd4a1e
Matt Hoffman <hofmadresu@gmail.com>
2023-01-22 18:19:21 -0500

    more company instructions plans

--- corporations/run-company.js
+++ corporations/run-company.js
@@ -16,24 +16,28 @@ export async function main(ns) {
     }
 
     while(true) {
-        // Get current industry (active && !(fullyExpanded && allEmployees))
         // for all divisions
             // If energy < 75 buy coffee
             // If morale or happiness < 60 throw party for 10_000_000
-        // if not fully expanded
-            // expand as able
-                // turn on smart supply
-                // start sales of correct stuff
-                // maybe buy some amount of multiplier equipment
-        // if not fully employeed but fully expanded
-            // expand office
-            // distribute employees
-        // if fully expanded
-            // Purchase any Unlocks or Upgrades we can afford
 
-        // if no current industry
-            // If can afford, start one
-            // If has product, create at 1_000_000_000/1_000_000_000
+        //allEmployees === 6??
+
+        // Get current industry (active && !(fullyExpanded && allEmployees))
+            // if not fully expanded
+                // expand as able
+                    // turn on smart supply
+                    // start sales of correct stuff
+                    // maybe buy some amount of multiplier equipment
+            // if not fully employeed but fully expanded
+                // expand office
+                // distribute employees
+            // if fully expanded
+                // Purchase any Unlocks or Upgrades we can afford
+            // If has product and fewer than 6, create at 1_000_000_000/1_000_000_000
+
+
+            // if no current industry
+                // If can afford, start one
 
         await ns.sleep(60000);
     }
