349ac71a32c3a9790c04d5b7bfe98e80377c8954
Matt Hoffman <hofmadresu@gmail.com>
2023-01-25 22:04:58 -0500

    fix manual-hack-self

--- experience/manual-hack-self.js
+++ experience/manual-hack-self.js
@@ -4,6 +4,7 @@ export async function main(ns) {
         const server = ns.singularity.getCurrentServer();
         if (ns.hasRootAccess(server)) {
             await ns.weaken(ns.getHostname());
+        } else if (ns.hasRootAccess(server)) { // This is silly, but makes sure we don't try to hack when we don't have access if we change servers between weaken and hack
             await ns.singularity.manualHack();
         } else {
             await ns.sleep(10);
