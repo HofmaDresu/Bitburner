d6f0bab4df98687a403097d7670d70e3b25c89b9
Matt Hoffman <hofmadresu@gmail.com>
2023-01-29 13:54:53 -0500

    fix bad code 🤦

--- experience/manual-hack-self.js
+++ experience/manual-hack-self.js
@@ -4,10 +4,12 @@ export async function main(ns) {
         const server = ns.singularity.getCurrentServer();
         if (ns.hasRootAccess(server)) {
             await ns.weaken(ns.getHostname());
-        } else if (ns.hasRootAccess(server)) { // This is silly, but makes sure we don't try to hack when we don't have access if we change servers between weaken and hack
+        } 
+        if (ns.hasRootAccess(server)) { // This is silly, but makes sure we don't try to hack when we don't have access if we change servers between weaken and hack
             await ns.singularity.manualHack();
-        } else {
-            await ns.sleep(10);
+        } 
+        if (!ns.hasRootAccess(server)) {
+            await ns.sleep(1000);
         }
     }
 }
