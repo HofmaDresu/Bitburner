c51411585e2ba07c525934b314924e91bd645f41
Matt Hoffman <hofmadresu@gmail.com>
2023-01-29 09:09:23 -0500

    adjust startup

--- startup.js
+++ startup.js
@@ -1,20 +1,19 @@
-import {getStartableServers, getBestServersForHacking} from "/helpers.js"
-
 /** @param {NS} ns */
 export async function main(ns) {
 	if (!ns.singularity.isBusy()) {
 		ns.singularity.universityCourse("Rothman University", "Study Computer Science", true);
 	}
 	ns.run('/money-maker/run-money-maker-v2.js');
+	ns.run('/contracts/complete-coding-contracts.js');
 	ns.run('/upgraders/purchase-server.js');
 	ns.run('/upgraders/purchase-programs.js');
 	ns.run('/upgraders/upgrade-home-server.js');
-	ns.run('/contracts/complete-coding-contracts.js');
 	if (ns.gang.inGang()) {
 		ns.run('/gang/run-gang.js');
 	}
-	ns.run('/upgraders/purchase-hacknet-nodes.js') 
+	//ns.run('/upgraders/purchase-hacknet-nodes.js') 
 	if (ns.getServerMaxRam("home") >= 1024) {
+		ns.run('/corporations/run-company.js');
 		ns.run('/stocks/stock-watcher.js');
 		ns.run('/stocks/activate-purchases.js');
 		ns.run('/stocks/play-the-market.js');
