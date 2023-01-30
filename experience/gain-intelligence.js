24d65b14eb8398b435cbc8d1a39433ead8b34f12
Matt Hoffman <hofmadresu@gmail.com>
2023-01-30 09:45:05 -0500

    fix gain intelligence 1+ run logic

--- experience/gain-intelligence.js
+++ experience/gain-intelligence.js
@@ -8,8 +8,9 @@ export async function main(ns) {
 	const manualHackRam = ns.getScriptRam('/experience/manual-hack-self.js');
 	const hackThreads = Math.min(Math.floor(availableMemory / manualHackRam), 1000);
 	const existingManualHacks = ns.ps("home").map(ps => ps.filename).filter(fn => fn === script).length
-	for(let i = existingManualHacks; i < hackThreads; i++) {
-		ns.run(script, 1);
+	for(let i = 0; i < hackThreads && existingManualHacks + i + 1 <= 1000; i++) {
+		ns.tprint(i)
+		ns.tprint(ns.run(script, 1));
 		await ns.sleep(1);
 	}
 }
