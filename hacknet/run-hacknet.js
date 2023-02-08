ddb53b50317b03ad6f59aeff27c65aa683824f3b
Matt Hoffman <hofmadresu@gmail.com>
2023-02-08 17:13:44 -0500

    helper + start smartening

--- hacknet/run-hacknet.js
+++ hacknet/run-hacknet.js
@@ -1,13 +1,24 @@
+import {getCheapestUpgrade} from 'hacknet/helpers';
+
 /** @param {NS} ns */
 export async function main(ns) {	
     while(true) {
-        const serverMoney = ns.getServerMoneyAvailable("home");
-        const sellCost = ns.hacknet.hashCost("Sell for Money");
+        const player = ns.getPlayer();
+        const currentNodes = ns.hacknet.numNodes();
+        const costToAddNode = ns.formulas.hacknetServers.hacknetServerCost(currentNodes+1, player.mults.hacknet_node_purchase_cost);
+        const cheapestUpgrade = getCheapestUpgrade(ns, currentNodes, player);
+        const cheepestUpgradeCost = Math.min((costToAddNode > 0 ? costToAddNode : Infinity), cheapestUpgrade);
+
         const currentHash = ns.hacknet.numHashes();
 
         //TODO: Much smarter, do more
-        if (sellCost < currentHash) {
-            ns.hacknet.spendHashes("Sell for Money");
+        if (cheepestUpgradeCost < 10_000_000) {
+            const sellCost = ns.hacknet.hashCost("Sell for Money");
+            if (sellCost < currentHash) {
+                ns.hacknet.spendHashes("Sell for Money");
+            }
+        } else {
+            
         }
 
 
--- upgraders/purchase-hacknet-servers.js
+++ upgraders/purchase-hacknet-servers.js
@@ -1,3 +1,5 @@
+import {getCheapestUpgrade} from 'hacknet/helpers';
+
 /** @param {NS} ns */
 export async function main(ns) {	
     const maxNodes = ns.hacknet.maxNumNodes();
@@ -33,31 +35,4 @@ export async function main(ns) {
 
         await ns.sleep(10_000);
     }
-}
-
-/** @param {NS} ns */
-function getCheapestUpgrade(ns, currentNodes, player) {
-    let cheapestUpgrade = ['none', 0, Infinity];
-
-    for (let i = 0; i < currentNodes; i++) {
-        const server = ns.hacknet.getNodeStats(i);
-        let cost = ns.formulas.hacknetServers.cacheUpgradeCost(server.cache, 1);
-        if (cost < cheapestUpgrade[2] && cost !== 0) {
-            cheapestUpgrade = ['cache', i, cost];
-        }
-        cost = ns.formulas.hacknetServers.coreUpgradeCost(server.cores, 1, player.mults.hacknet_node_core_cost);
-        if (cost < cheapestUpgrade[2] && cost !== 0) {
-            cheapestUpgrade = ['core', i, cost];
-        }
-        cost = ns.formulas.hacknetServers.levelUpgradeCost(server.level, 1, player.mults.hacknet_node_level_cost);
-        if (cost < cheapestUpgrade[2] && cost !== 0) {
-            cheapestUpgrade = ['level', i, cost];
-        }
-        cost = ns.formulas.hacknetServers.ramUpgradeCost(server.ram, 1, player.mults.hacknet_node_ram_cost);
-        if (cost < cheapestUpgrade[2] && cost !== 0) {
-            cheapestUpgrade = ['ram', i, cost];
-        }
-    }
-
-    return cheapestUpgrade;
 }
