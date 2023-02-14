ade31f1a97e94ab0566ec7513cf29d8e204b5895
Matt Hoffman <hofmadresu@gmail.com>
2023-02-13 19:48:52 -0500

    smarter purchases

--- hacknet/run-hacknet.js
+++ hacknet/run-hacknet.js
@@ -19,18 +19,18 @@ export async function main(ns) {
         const currentHash = ns.hacknet.numHashes();
         const myHackingLevel = ns.getHackingLevel();
         const orderedHackingServers = getBestServersForHacking(ns, myHackingLevel).map(s => ns.getServer(s));
-        const orderedNotMinSecurityServers = orderedHackingServers.filter(s => s.minDifficulty > 1).map(s => s.hostname);
-        const orderedNotMaxMoneyServers = orderedHackingServers.filter(s => s.maxMoney < 10_000_000_000_000 * multipliers.ServerMaxMoney).map(s => s.hostname);
+        const orderedNotMinSecurityServers = orderedHackingServers.filter(s => s.minDifficulty > 1).sort((a, b) => b.minDifficulty - a.minDifficulty).map(s => s.hostname);
+        const orderedNotMaxMoneyServers = orderedHackingServers.filter(s => s.moneyMax < 10_000_000_000_000 * multipliers.ServerMaxMoney).map(s => s.hostname);
 
         const maxHashes = ns.hacknet.hashCapacity();
         const costToDecreaseSecurity = ns.hacknet.hashCost("Reduce Minimum Security");
         const costToBoostMoney = ns.hacknet.hashCost("Increase Maximum Money");
 
-        if (cheepestUpgradeCost < 10_000_000) {
+        if (cheepestUpgradeCost < 20_000_000) {
             if (ns.hacknet.hashCost("Sell for Money") < currentHash) {
                 ns.hacknet.spendHashes("Sell for Money");
             }
-        } else if (orderedNotMinSecurityServers.length > 0 && costToDecreaseSecurity <= maxHashes) {
+        } else if (orderedNotMinSecurityServers.length > 0 && costToDecreaseSecurity <= maxHashes && (orderedNotMaxMoneyServers.length == 0 || costToDecreaseSecurity < 2 * costToBoostMoney)) {
             if (costToDecreaseSecurity < currentHash) {
                 ns.hacknet.spendHashes("Reduce Minimum Security", orderedNotMinSecurityServers[0]);
             }
