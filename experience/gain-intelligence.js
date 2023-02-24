2e5f61f738a62b20634a7cd90a05252d8a80dab7
Matt Hoffman <hofmadresu@gmail.com>
2023-02-24 07:52:16 -0500

    remove todo

--- experience/gain-intelligence.js
+++ experience/gain-intelligence.js
@@ -9,7 +9,6 @@ export async function main(ns) {
 	const hackThreads = Math.min(Math.floor(availableMemory / manualHackRam), 1000);
 	const existingManualHacks = ns.ps("home").map(ps => ps.filename).filter(fn => fn === script).length
 	for(let i = 0; i < hackThreads && existingManualHacks + i + 1 <= 1000; i++) {
-		//TODO: add uuid for unique runs
 		ns.run(script, 1, crypto.randomUUID());
 		await ns.sleep(1);
 	}
