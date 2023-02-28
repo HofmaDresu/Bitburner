98d0835a4fc0098053c4219ee18b9f7fd1e8877d
Matt Hoffman <hofmadresu@gmail.com>
2023-02-28 09:30:55 -0500

    even more hack experience

--- automation/script-starter.js
+++ automation/script-starter.js
@@ -1,4 +1,5 @@
 import {getMaxContractSolverRam} from 'helpers';
+import { MAX_MANUAL_HACKS, MANUAL_HACK_SCRIPT } from 'experience/gain-intelligence';
 
 /** @param {NS} ns */
 export async function main(ns) {
@@ -19,14 +20,17 @@ export async function main(ns) {
             //TODO: make sure we're in slum snakes
             ns.gang.createGang("Slum Snakes");
         }
+        // TODO: if hacking is too weak, start hacknet servers
         startScriptIfAvailableRam(ns, '/upgraders/purchase-hacknet-servers.js');
         startScriptIfAvailableRam(ns, '/hacknet/run-hacknet.js');
-        // TODO: spend hashes
         startScriptIfAvailableRam(ns, '/stocks/stock-watcher.js');
         startScriptIfAvailableRam(ns, '/stocks/activate-purchases.js');
         startScriptIfAvailableRam(ns, '/stocks/play-the-market.js');
         startScriptIfAvailableRam(ns, '/corporations/run-company.js');
         startScriptIfAvailableRam(ns, '/experience/gain-intelligence.js');
+        if (allScriptsAreRunning(ns)) {
+            startScriptIfAvailableRam(ns, '/experience/gain-hack-experience.js');
+        }
         await ns.sleep(10_000);
     }
 }
@@ -34,11 +38,27 @@ export async function main(ns) {
 /** @param {NS} ns */
 function startScriptIfAvailableRam(ns, script) {
     const maxContractSolverRam = getMaxContractSolverRam(ns);
-    const availableRam = ns.getServerMaxRam("home") - ns.getServerUsedRam("home") - maxContractSolverRam;
+    const availableRam = ns.getServerMaxRam("home") - ns.getServerUsedRam("home") - maxContractSolverRam - ns.getScriptRam("/money-maker/start-server.js");
     const scriptRam = ns.getScriptRam(script);
     const isScriptRunning = ns.scriptRunning(script, "home");
 
     if (scriptRam < availableRam && !isScriptRunning) {
         ns.run(script);
     }
+}
+
+// NOTE: Skips purchase programs and purchase server because those can finish
+/** @param {NS} ns */
+function allScriptsAreRunning(ns) {
+    return ns.scriptRunning('/money-maker/run-money-maker-v2.js', "home") &&
+        ns.scriptRunning('/upgraders/upgrade-home-server.js', "home") &&
+        ns.scriptRunning('/contracts/complete-coding-contracts.js', "home") &&
+        ns.scriptRunning('/gang/run-gang.js', "home") &&
+        ns.scriptRunning('/upgraders/purchase-hacknet-servers.js', "home") &&
+        ns.scriptRunning('/hacknet/run-hacknet.js', "home") &&
+        ns.scriptRunning('/stocks/stock-watcher.js', "home") &&
+        ns.scriptRunning('/stocks/play-the-market.js', "home") &&
+        ns.scriptRunning('/corporations/run-company.js', "home") &&
+        ns.scriptRunning('/experience/gain-intelligence.js', "home") &&
+        ns.ps("home").filter(ps => ps.filename === MANUAL_HACK_SCRIPT).length === MAX_MANUAL_HACKS;
 }
--- experience/gain-intelligence.js
+++ experience/gain-intelligence.js
@@ -1,15 +1,17 @@
 import {getMaxContractSolverRam} from 'helpers';
 
+export const MAX_MANUAL_HACKS = 1000;
+export const MANUAL_HACK_SCRIPT = "/experience/manual-hack-self.js";
+
 /** @param {NS} ns */
 export async function main(ns) {
-	const script = "/experience/manual-hack-self.js";
 	const minMemory = Math.max(ns.getServerMaxRam("home") * .1, getMaxContractSolverRam(ns));
 	let availableMemory = Math.max(ns.getServerMaxRam("home") - ns.getServerUsedRam("home") - minMemory, 0);
-	const manualHackRam = ns.getScriptRam('/experience/manual-hack-self.js');
-	const hackThreads = Math.min(Math.floor(availableMemory / manualHackRam), 1000);
-	const existingManualHacks = ns.ps("home").map(ps => ps.filename).filter(fn => fn === script).length
-	for(let i = 0; i < hackThreads && existingManualHacks + i + 1 <= 1000; i++) {
-		ns.run(script, 1, crypto.randomUUID());
+	const manualHackRam = ns.getScriptRam(MANUAL_HACK_SCRIPT);
+	const hackThreads = Math.min(Math.floor(availableMemory / manualHackRam), MAX_MANUAL_HACKS);
+	const existingManualHacks = ns.ps("home").map(ps => ps.filename).filter(fn => fn === MANUAL_HACK_SCRIPT).length
+	for(let i = 0; i < hackThreads && existingManualHacks + i + 1 <= MAX_MANUAL_HACKS; i++) {
+		ns.run(MANUAL_HACK_SCRIPT, 1, crypto.randomUUID());
 		await ns.sleep(1);
 	}
 }
