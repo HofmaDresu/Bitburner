85a10fdde1d17771a3ba545f2b6e1ac85704da5b
hofmadresu <hofmadresu@gmail.com>
2023-03-19 10:29:52 -0400

    add percent

--- stocks/get-portfolio.js
+++ stocks/get-portfolio.js
@@ -10,7 +10,7 @@ export async function main(ns) {
 		value += ns.stock.getSaleGain(stockSymbol, shortShares, "Short");
         const cost = longShares * longPx + shortShares * shortPx;
         if (value !== 0) {
-            ns.tprint(`${stockSymbol}: value -> ${value.toLocaleString('en-US')}, profit -> ${(value - cost).toLocaleString('en-US')}`);
+            ns.tprint(`${stockSymbol}: value -> ${value.toLocaleString('en-US')}, profit -> ${(value - cost).toLocaleString('en-US')}, pct -> ${((value - cost) / cost * 100).toLocaleString('en-US')}%`);
         };
 	});
 }
