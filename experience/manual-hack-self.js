78c07c8c000a4c767e7048c82e3c4d54aac4d262
Matt Hoffman <hofmadresu@gmail.com>
2023-03-21 09:38:42 -0400

    reset server on manual hack self

--- experience/manual-hack-self.js
+++ experience/manual-hack-self.js
@@ -1,13 +1,15 @@
 /** @param {NS} ns */
 export async function main(ns) {
     while(true) {
-        const server = ns.singularity.getCurrentServer();
+        let server = ns.singularity.getCurrentServer();
         if (ns.hasRootAccess(server)) {
             await ns.weaken(ns.getHostname());
         } 
+        server = ns.singularity.getCurrentServer();
         if (ns.hasRootAccess(server)) { // This is silly, but makes sure we don't try to hack when we don't have access if we change servers between weaken and hack
             await ns.singularity.manualHack();
         } 
+        server = ns.singularity.getCurrentServer();
         if (!ns.hasRootAccess(server)) {
             await ns.sleep(1000);
         }
