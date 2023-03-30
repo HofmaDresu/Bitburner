7091db5f7e10bdc15fca23a0f53890819c95b5b0
Matt Hoffman <hofmadresu@gmail.com>
2023-03-29 23:36:52 -0400

    more stock info for decisions

--- stocks/get-current-sale-value.js
+++ stocks/get-current-sale-value.js
@@ -2,7 +2,11 @@ import {stockToServers} from "/stocks/helpers.js"
 
 /** @param {NS} ns */
 export async function main(ns) {
-	var currentSaleGain = Object.keys(stockToServers).map(stockSymbol => {
+	const purchasePrice = Object.keys(stockToServers).map(stockSymbol => {
+		const [longShares, longPx, shortShares, shortPx] = ns.stock.getPosition(stockSymbol);
+		return longPx * longShares + shortPx * shortShares;
+	}).reduce((prev, curr) => prev += curr, 0);
+	const currentSaleGain = Object.keys(stockToServers).map(stockSymbol => {
 		const [longShares, _longPx, shortShares, _shortPx] = ns.stock.getPosition(stockSymbol);
 		let value = 0;
 		
@@ -10,5 +14,15 @@ export async function main(ns) {
 		value += ns.stock.getSaleGain(stockSymbol, shortShares, "Short");
 		return value;
 	}).reduce((prev, curr) => prev += curr, 0);
-	ns.tprint(currentSaleGain.toLocaleString('en-US'));
+	const currentPositiveSaleGain = Object.keys(stockToServers).map(stockSymbol => {
+		const [longShares, _longPx, shortShares, _shortPx] = ns.stock.getPosition(stockSymbol);
+		let value = 0;
+		
+		value += ns.stock.getSaleGain(stockSymbol, longShares, "Long");
+		value += ns.stock.getSaleGain(stockSymbol, shortShares, "Short");
+		return value;
+	}).filter(x => x > 0).reduce((prev, curr) => prev += curr, 0);
+	ns.tprint(`Purchased: ${purchasePrice.toLocaleString('en-US')}`);
+	ns.tprint(`Total: ${currentSaleGain.toLocaleString('en-US')}`);
+	ns.tprint(`Positive: ${currentPositiveSaleGain.toLocaleString('en-US')}`);
 }
