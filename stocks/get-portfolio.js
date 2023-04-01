d852cdad2a93890ea6bc049de3a065d989c07468
Matt Hoffman <hofmadresu@gmail.com>
2023-04-01 09:39:14 -0400

    more portfolio info

--- stocks/get-portfolio.js
+++ stocks/get-portfolio.js
@@ -2,15 +2,21 @@ import {stockToServers} from "stocks/helpers"
 
 /** @param {NS} ns */
 export async function main(ns) {
-	Object.keys(stockToServers).forEach(stockSymbol => {
-		const [longShares, longPx, shortShares, shortPx] = ns.stock.getPosition(stockSymbol);
-		let value = 0;
-		
-		value += ns.stock.getSaleGain(stockSymbol, longShares, "Long");
-		value += ns.stock.getSaleGain(stockSymbol, shortShares, "Short");
-        const cost = longShares * longPx + shortShares * shortPx;
-        if (value !== 0) {
-            ns.tprint(`${stockSymbol}: value -> ${value.toLocaleString('en-US')}, position -> ${longShares > 0 ? "L" : ""}${shortShares > 0 ? "S" : ""}, profit -> ${(value - cost).toLocaleString('en-US')}, pct -> ${((value - cost) / cost * 100).toLocaleString('en-US')}%`);
-        };
-	});
+	Object.keys(stockToServers)
+		.map(stockSymbol => {
+			const [longShares, longPx, shortShares, shortPx] = ns.stock.getPosition(stockSymbol);
+			let value = 0;
+			
+			value += ns.stock.getSaleGain(stockSymbol, longShares, "Long");
+			value += ns.stock.getSaleGain(stockSymbol, shortShares, "Short");
+			const cost = longShares * longPx + shortShares * shortPx;
+
+			return {stockSymbol, value, cost, longShares, shortShares};
+		})
+		.sort((a, b) => b.value - a.value)
+		.forEach(info => {
+			if (info.value !== 0) {
+				ns.tprint(`${info.stockSymbol}: value -> ${info.value.toLocaleString('en-US')}, position -> ${info.longShares > 0 ? "L" : ""}${info.shortShares > 0 ? "S" : ""}, profit -> ${(info.value - info.cost).toLocaleString('en-US')}, pct -> ${((info.value - info.cost) / info.cost * 100).toLocaleString('en-US')}%`);
+			};
+		});
 }
