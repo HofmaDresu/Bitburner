eb11cda7fefae5db28f08272e72f4ce470986042
Hofma Dresu <hofmadresu@gmail.com>
2025-09-20 12:50:48 -0400

    make work

--- gameCode/control/makeServersSelfHack.js
+++ gameCode/control/makeServersSelfHack.js
@@ -1,12 +1,6 @@
-/* server {
-    name: string,
-    
-
-} */
-
-
 /** @param {NS} ns */
 export async function main(ns) {
+    ns.disableLog("disableLog");
     ns.disableLog("asleep");
     ns.disableLog("sleep");
     ns.disableLog("scp");
@@ -15,40 +9,45 @@ export async function main(ns) {
     ns.disableLog("getServerNumPortsRequired");
     ns.disableLog("brutessh");
     ns.disableLog("nuke");
-    ns.disableLog("scp");
-    ns.disableLog("scp");
+    ns.disableLog("exec");
+    ns.disableLog("scan");
     ns.ui.openTail();
     await ns.asleep(1000);
 
+    const servers = getServers(ns);
+    ns.print(servers);
 
-    //while(true) {
-        dfsServers(ns, "", "home");
-
-//        await ns.asleep(10000);
-  //  }
-
+    while(true) {
+        startServers(ns, servers);
+        await ns.asleep(10000);
+    }
 }
 
 /** @param {NS} ns */
-async function dfsServers(ns, parent, current) {
-        ns.print(`New run with parent ${parent}, current ${current}`)
+function getServers(ns) {
+    let servers = ns.scan("home").filter((s) => s !== parent && s.indexOf("pserv") === -1);
+    let currentServersLength = 0;
+    let iterator = 0;
+    do {
+        const newServers = ns.scan(servers[iterator]).filter((s) => servers.indexOf(s) === -1 && s !== "home");
+        servers = servers.concat(newServers);
+        currentServersLength = servers.length;
+        iterator++;
+    } while (iterator < currentServersLength);
 
-    const newServers = ns.scan(current).filter((s) => s !== parent && s.indexOf("pserv") === -1);
-    ns.print(`newServers: ${newServers}`)
-    if (newServers.length > 0) {
-        for (let server of newServers) {    
-            ns.print(`Looping with parent ${parent}, current ${current} and server ${server}`)
-            startServerIfPossible(ns, server);
-            //await dfsServers(ns, current, server)
-            // await ns.asleep(1000);
-        };
-    }
+    return servers;    
+}
+
+/** @param {NS} ns */
+async function startServers(ns, servers) {
+    for (let server of servers) {
+        startServerIfPossible(ns, server);
+    };
     await ns.asleep(1000);
 }
 
 /** @param {NS} ns */
  function startServerIfPossible(ns, server) {
-    ns.print(`Attempting to start server '${server}'`)
     const moneyThresh = ns.getServerMaxMoney(server);
     const player = ns.getPlayer();
     const requiredHackingLevel = ns.getServerRequiredHackingLevel(server);
@@ -56,7 +55,6 @@ async function dfsServers(ns, parent, current) {
 
     if (player.skills.hacking < requiredHackingLevel) return;
     if (moneyThresh === 0) return;
-    //TODO skip if already running
 
     let portCount = 0;
     if (ns.fileExists("BruteSSH.exe", "home")) {
@@ -64,9 +62,13 @@ async function dfsServers(ns, parent, current) {
         portCount++;
     }
 
+
     if (portCount < requiredNumPorts) return;
-    ns.nuke(server);    
+    ns.nuke(server);
     
-    ns.scp(["control/makeMoneyFromTarget.js", "growing/growTargetToMax.js", "hacking/hackTarget.js", "weakening/weakenTargetToMin.js"], server);
-    ns.exec("control/makeMoneyFromTarget.js", server, 1, server);
+    const mainScript = "control/makeMoneyFromTarget.js";
+    if (ns.scriptRunning)
+    ns.scp([mainScript, "growing/growTargetToMax.js", "hacking/hackTarget.js", "weakening/weakenTargetToMin.js"], server);
+    ns.print(`Attempting to start server '${server}'`)
+    ns.exec(mainScript, server, 1, server);
 }
