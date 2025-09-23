d5a8a90eb720aea839082f70155a4eb2aa8a5aeb
Hofma Dresu <hofmadresu@gmail.com>
2025-09-23 17:30:37 -0400

    re-simplify for now

--- gameCode/control/homeRunner.js
+++ gameCode/control/homeRunner.js
@@ -1,27 +1,19 @@
-import { startScriptOnHomeIfAble, killScriptIfRunningOnHome, getAvailableRam, getConfig, CONFIG_BUY_STOCKS, getBestServerToHack } from "helpers";
+import { startScriptOnHomeIfAble, killScriptIfRunningOnHome, getAvailableRam } from "helpers";
 import { canTradeStocks } from "stocks/helpers";
 
 /** @param {NS} ns */
 export async function main(ns) {
     ns.disableLog("sleep");
-    let availableRam = getAvailableRam(ns, "home");
+    let availableRam = getAvailableRam(ns, "home");   
+    let thingsToDo = true;
     // TODO: restart makeMoneyFromTarget and servers when new best target exists
-    // TODO: make actually run the makeServers stuff instead of farming out, they clash with each other
     while (true) {
-        if (canTradeStocks(ns) && ns.getPlayer().skills.hacking > 100 && getConfig(ns)[CONFIG_BUY_STOCKS]) {
+        if (canTradeStocks(ns) && ns.getPlayer().skills.hacking > 100) {
             killScriptIfRunningOnHome(ns, "control/makeServersSelfHack.js");
             killScriptIfRunningOnHome(ns, "control/makeMoneyFromTarget.js");
             startScriptOnHomeIfAble(ns, "control/makeServersManipulateMarket.js", availableRam)
             startScriptOnHomeIfAble(ns, "stocks/manipulateTheMarket.js", availableRam)
-            continue;
-        }
-
-        if (!getConfig(ns)[CONFIG_BUY_STOCKS]) {
-            killScriptIfRunningOnHome(ns, "control/makeServersManipulateMarket.js");
-            killScriptIfRunningOnHome(ns, "stocks/manipulateTheMarket.js");
-            startScriptOnHomeIfAble(ns, "control/makeServersSelfHack.js", availableRam);
-            startScriptOnHomeIfAble(ns, "control/makeMoneyFromTarget.js", availableRam, [getBestServerToHack(ns)]);
-
+            thingsToDo = false;
         }
 
         await ns.sleep(10_000);
