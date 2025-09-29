dcdd17dc6621e059948d7274ba30de70607e662d
Hofma Dresu <hofmadresu@gmail.com>
2025-09-29 18:24:21 -0400

    max threads on n00dles

--- gameCode/test.js
+++ gameCode/test.js
@@ -5,7 +5,5 @@ import { getConfig } from "helpers";
 /** @param {NS} ns */
 export async function main(ns) {
     ns.ui.openTail();
-    const foo = getConfig(ns);
-    ns.print(ns.getMoneySources().sinceInstall.hacknet * 100);
-    ns.print(ns.getMoneySources().sinceInstall.hacking);
+    ns.print(ns.flags([["canUseAllMoney", false]])["canUseAllMoney"]);
 }
