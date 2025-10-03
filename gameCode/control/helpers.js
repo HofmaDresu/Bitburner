a3a9d1ef1beaa3348d65e3d502dd54083e55f68d
Hofma Dresu <hofmadresu@gmail.com>
2025-10-03 08:52:20 -0400

    only backdoor what's needed

--- gameCode/control/helpers.js
+++ gameCode/control/helpers.js
@@ -1,5 +1,7 @@
 import { nukeServer, getServersTree } from "helpers";
 
+const SERVERS_TO_BACKDOOR = ["CSEC", "avmnite-02h", "I.I.I.I", "run4theh111z", "The-Cave", "w0rld_d43m0n"];
+
 /** @param {NS} ns */
 export async function crackServers(ns) {
     const serverTree = getServersTree(ns, "home")
@@ -10,7 +12,7 @@ export async function crackServers(ns) {
 async function crackServersInTree(ns, serverTree, parent = "home") {
     for (const server in serverTree) {
         ns.singularity.connect(server)
-        if(nukeServer(ns, server) && !ns.getServer(server).backdoorInstalled && server !== "home") {   
+        if(nukeServer(ns, server) && !ns.getServer(server).backdoorInstalled && SERVERS_TO_BACKDOOR.indexOf(server) !== -1) {   
             await ns.singularity.installBackdoor();            
         }
         const leaf = serverTree[server];
