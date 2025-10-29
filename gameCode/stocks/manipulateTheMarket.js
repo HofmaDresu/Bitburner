086fe0d0e635c3a8ed694a78f23e601e41b655c0
Hofma Dresu <hofmadresu@gmail.com>
2025-10-29 08:45:55 -0400

    loosen manipulation bounds

--- gameCode/stocks/manipulateTheMarket.js
+++ gameCode/stocks/manipulateTheMarket.js
@@ -56,7 +56,7 @@ export async function main(ns) {
                     if (ns.getServerSecurityLevel(target) > securityThresh) {
                         await weaken(ns, hostname, [target]);
                     } else  {
-                        if (ns.getServerMoneyAvailable(target) < .1 * ns.getServerMaxMoney(target)) {
+                        if (ns.getServerMoneyAvailable(target) < .5 * ns.getServerMaxMoney(target)) {
                             await grow(ns, hostname, [target, true]);
                         } else {
                             await hackTargetToMin(ns, hostname, target);
@@ -66,7 +66,7 @@ export async function main(ns) {
                     if (ns.getServerSecurityLevel(target) > securityThresh) {
                         await weaken(ns, hostname, [target]);
                     } else {        
-                        if (ns.getServerMoneyAvailable(target) > .1 * ns.getServerMaxMoney(target)) {
+                        if (ns.getServerMoneyAvailable(target) > .5 * ns.getServerMaxMoney(target)) {
                             await hack(ns, hostname, [target, true]);
                         } else {
                             await growTargetToMax(ns, hostname, target);
