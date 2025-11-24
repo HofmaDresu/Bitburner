adba75233db90a8ef3928d518c645eaa218f3a4c
Hofma Dresu <hofmadresu@gmail.com>
2025-11-24 17:16:50 -0500

    Add logs

--- gameCode/stocks/manipulateTheMarket.js
+++ gameCode/stocks/manipulateTheMarket.js
@@ -57,14 +57,18 @@ export async function main(ns) {
                 }
                 if (sharesLong) {
                     if (ns.getServerMoneyAvailable(target) < .5 * ns.getServerMaxMoney(target)) {
+                        ns.print(`Growing + manipulating ${symbol} because we own ${sharesLong} stocks`)
                         await grow(ns, hostname, [target, true]);
                     } else {
+                        ns.print(`Just hacking ${symbol} because we own ${sharesLong} stocks and money available is too high`)
                         await hack(ns, hostname, [target]);
                     }
                 } else {    
                     if (ns.getServerMoneyAvailable(target) > .5 * ns.getServerMaxMoney(target)) {
+                        ns.print(`Hacking + manipulating ${symbol} because we do not own long stocks`)
                         await hack(ns, hostname, [target, true]);
                     } else {
+                        ns.print(`Just growing ${symbol} because we do not own long stocks and money available is too high`)
                         await grow(ns, hostname, [target]);
                     }            
                 }
